034f2930c31642bf371189e9149d454d96f54296
ukssss <1872003115su@gmail.com>
2022-08-18 18:53:39 +0900

    2022.08.18 18:52 MainActivity2 리사이클러뷰 전환 완료 (ListView -> RecyclerView) 레이아웃 수정, 검색 필터 기능 추가 예정

--- app/src/main/java/com/example/parking/MainActivity2.java
+++ app/src/main/java/com/example/parking/MainActivity2.java
@@ -1,39 +1,20 @@
 package com.example.parking;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.StrictMode;
 import android.util.Log;
 import android.view.Gravity;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.BaseAdapter;
-import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.ListView;
-import android.widget.Spinner;
-import android.widget.SpinnerAdapter;
-import android.widget.TextView;
-import android.widget.Toast;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
 
-import org.xmlpull.v1.XmlPullParser;
-import org.xmlpull.v1.XmlPullParserFactory;
-
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -46,15 +27,11 @@ public class MainActivity2 extends AppCompatActivity {
     private DrawerLayout drawerLayout;
     private NavigationView nav;
 
-    TextView text;
-    TextView tv_spinner;
-    Spinner spinner;
-    String data;
-
-    XmlPullParser xpp;
     String key = "iUIvfulYGP2WncxaP93CKSBBGWPWdo5JDw7Ci7aLBbYni3pvsQ1VNvuJKhXF7sQ910XZu1lT%2FSX1aBtLdZ6xTA%3D%3D";
-    String area;
-    String areaNum;
+
+    private RecyclerView recyclerView;
+    private LinearLayoutManager linearLayoutManager;
+    private RecyclerAdapter adapter;
 
     @Override
     protected void onNewIntent(Intent intent) {
@@ -67,17 +44,19 @@ public class MainActivity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
-        init();
+        initNavigationView();
         onClickDrawer();
         NavigationViewHelper.enableNavigation(mContext, nav);
 
         // Load Database (Parkinglot)
         List<Parkinglot> parkinglotList = initLoadParkinglotDatabase();
 
-        getParkinglotData(parkinglotList);
+        initializedRecycler(parkinglotList);
+
+
     }
 
-    private void init() {
+    private void initNavigationView() {
         nav = findViewById(R.id.nav);
     }
 
@@ -100,31 +79,17 @@ public class MainActivity2 extends AppCompatActivity {
         return parkinglotList;
     }
 
-    private void getParkinglotData(List<Parkinglot> parkinglotList) {
-
-        ArrayList<String> parkinglot = new ArrayList<String>();
-        ListView listView = (ListView) findViewById(R.id.listview);
+    private void initializedRecycler(List<Parkinglot> parkinglotList) {
+        recyclerView = findViewById(R.id.recyclerView);
+        linearLayoutManager = new LinearLayoutManager(this);
+        adapter = new RecyclerAdapter();
 
         for (int i = 0; i < parkinglotList.size(); i++) {
-
-            String title = parkinglotList.get(i).prkplceNm;
-            String subTitle = parkinglotList.get(i).lnmadr;
-            double latitude = parkinglotList.get(i).latitude;
-            double longitude = parkinglotList.get(i).longitude;
-
-            parkinglot.add(
-                    "주차장명 : " + title + "\n" +
-                            "주소 : " + subTitle + "\n" +
-                            "위도 : " + latitude + "\n" +
-                            "경도 : " + longitude + "\n\n"
-            );
-
+            adapter.addItems(parkinglotList.get(i));
         }
 
-        ArrayAdapter adapter = new ArrayAdapter(getApplicationContext(), android.R.layout.simple_list_item_1, parkinglot);
-
-        listView.setAdapter(adapter);
-
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setAdapter(adapter);
     }
 }
 
---
+++ app/src/main/java/com/example/parking/RecyclerAdapter.java
@@ -0,0 +1,63 @@
+package com.example.parking;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.MyViewHolder> {
+
+    private ArrayList<Parkinglot> parkinglotData = new ArrayList<>();
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.parkinglot_listview, parent, false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        holder.onBind(parkinglotData.get(position), position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return parkinglotData.size();
+    }
+
+    public void addItems(Parkinglot parkinglot) {
+        parkinglotData.add(parkinglot);
+        notifyDataSetChanged();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        private TextView id;
+        private TextView name;
+        private TextView addr;
+        private TextView div;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            id = itemView.findViewById(R.id.id);
+            name = itemView.findViewById(R.id.name);
+            addr = itemView.findViewById(R.id.addr);
+            div = itemView.findViewById(R.id.div);
+        }
+
+        public void onBind(Parkinglot parkinglot, int position) {
+            String s = "" + (position + 1);
+            id.setText(s);
+            name.setText(parkinglot.getName());
+            addr.setText(parkinglot.getAddr());
+            div.setText(parkinglot.getDiv());
+
+        }
+    }
+}
